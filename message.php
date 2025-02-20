017f802c4dd9a04363e17f67503e4e0f99dcc41d
morgana <89588301+morgana-x@users.noreply.github.com>
2025-02-20 23:40:47 +1100

    Delete message.php

--- message.php
+++
@@ -1,8 +0,0 @@
-<?php
-        session_start();
-        include("config.php");
-        include("scripts/util.php");
-        $thread = getCurrentThread();
-        $page = print_pages($thread);
-        printMessages($thread, $page);
-?>
